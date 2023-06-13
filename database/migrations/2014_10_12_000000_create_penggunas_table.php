e4d7c3b8a91c742774c31bfb95742e9729d74099
Winzliu <alwin.liufandy@gmail.com>
2023-06-13 16:33:56 +0700

    Membuat seeder dan memperbaiki migration, NB: Model blm lengkap

--- database/migrations/2014_10_12_000000_create_penggunas_table.php
+++ database/migrations/2014_10_12_000000_create_penggunas_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('penggunas', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
             $table->string('jenisKelamin');
@@ -30,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('penggunas');
     }
 };
